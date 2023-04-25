4ace703b880a8788ecd8bea28cede53771aa0e22
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-04-25 17:24:05 -0500

    add add_input method for tracking outside conversation history

--- app/chatbot.py
+++ app/chatbot.py
@@ -20,7 +20,7 @@ class Chatbot:
             max_length=200,
         )
 
-    def input(self, user_input: str) -> str:
+    def input_to_response(self, user_input: str) -> str:
         """Send user input to the model for text generation
 
         Args:
@@ -30,10 +30,21 @@ class Chatbot:
             str: Generated response
         """
 
-        self.conv.add_user_input(text=user_input)
+        self.conv.add_user_input(text=user_input, overwrite=True)
         self.conv = self.chatbot(self.conv)
 
         return self.conv.generated_responses[-1]
+
+    def add_input(self, user_input: str) -> None:
+        """Adds contextual messages from the chat to conversation cache.
+        The message will still be processed for context but the response is thrown away.
+
+        Args:
+            user_input (str): Message to be cached
+        """
+
+        self.conv.add_user_input(text=user_input)
+        self.conv = self.chatbot(self.conv)
 
     def get_response_hist(self) -> list:
         """Getter for response history
@@ -53,13 +64,19 @@ class Chatbot:
 
         return self.conv.past_user_inputs
 
-    def rm_outdated_hist(self) -> None:
-        """Removes the oldest inputs and responses for model focus and efficiency"""
+    def rm_oldest_input(self) -> None:
+        """Removes the oldest input"""
 
         self.conv.past_user_inputs.pop(0)
+
+        print("Oldest input removed")
+
+    def rm_oldest_response(self) -> None:
+        """Removes the oldest response"""
+
         self.conv.generated_responses.pop(0)
 
-        print("Oldest input and response removed")
+        print("Oldest response removed")
 
     def reset_hist(self) -> None:
         """Resets the conversation history when called"""
