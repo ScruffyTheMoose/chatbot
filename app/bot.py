c719b921b57b14d29ef4457b43bf019f7ea9279f
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-04-25 17:25:44 -0500

    add compatibility with outside dialogue tracking which is terrible dont use this ew gross stinky

--- app/bot.py
+++ app/bot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, random
 from chatbot import Chatbot
 
 intents = discord.Intents.all()
@@ -20,28 +20,46 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("~!"):
-
-        print(f"Message recieved: {message.content}")
+    if message.content.startswith("~!") or random.randint(0, 100) < 10:
 
         # remove '~!' from start of message
-        user_message = message.content[2:]
+        if message.content.startswith("~!"):
+            user_message = message.content[2:]
+        else:
+            user_message = message.content
+
+        # confirming input recieved correctly
+        print(f"User: {user_message}")
 
         # allows any user to clear the chat history
         if user_message == "RESET":
             chatbot.reset_hist()
             return
 
-        print(user_message)
-
         # generate response from chatbot
-        response = chatbot.input(user_message)
-        print(response)
+        response = chatbot.input_to_response(user_input=user_message)
+
+        # confirming response sent correctly
+        print(f"Bot: {response}")
 
         # Do something with user_input_text here
         await message.channel.send(response)
 
-        print("Response sent.")
+    # tracks contextual messages being sent in the chat
+    else:
+        user_message = message.content
+        chatbot.add_input(user_input=user_message)
+
+        # confirming input tracked
+        print(f"Tracked: {user_message}")
+
+    # clearing oldest input
+    if len(chatbot.get_input_hist()) > 10:
+        chatbot.rm_oldest_input()
+
+    # clearing oldest response
+    if len(chatbot.get_response_hist()) > 10:
+        chatbot.rm_oldest_response()
 
 
 # I pushed an old key alright don't judge me
