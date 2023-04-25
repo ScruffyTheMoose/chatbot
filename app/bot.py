f8eddf5683bee805a75e68605ea2683e692a8d36
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-04-25 18:11:05 -0500

    major refactor to implement new build of conversational model

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,5 @@
 import discord, random
-from chatbot import Chatbot
+from model import Chatbot, Preprocess
 
 intents = discord.Intents.all()
 intents.members = False
@@ -20,46 +20,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("~!") or random.randint(0, 100) < 10:
+    if message.content.startswith("~!"):
 
-        # remove '~!' from start of message
-        if message.content.startswith("~!"):
-            user_message = message.content[2:]
-        else:
-            user_message = message.content
+        # remove '~!' from start of message and sanitizes contents
+        user_message = Preprocess.sanitize_input(message.content[2:])
 
         # confirming input recieved correctly
         print(f"User: {user_message}")
 
-        # allows any user to clear the chat history
-        if user_message == "RESET":
-            chatbot.reset_hist()
-            return
-
         # generate response from chatbot
-        response = chatbot.input_to_response(user_input=user_message)
+        response = chatbot.run(input=user_message)
 
         # confirming response sent correctly
         print(f"Bot: {response}")
 
         # Do something with user_input_text here
         await message.channel.send(response)
-
-    # tracks contextual messages being sent in the chat
-    else:
-        user_message = message.content
-        chatbot.add_input(user_input=user_message)
-
-        # confirming input tracked
-        print(f"Tracked: {user_message}")
-
-    # clearing oldest input
-    if len(chatbot.get_input_hist()) > 10:
-        chatbot.rm_oldest_input()
-
-    # clearing oldest response
-    if len(chatbot.get_response_hist()) > 10:
-        chatbot.rm_oldest_response()
 
 
 # I pushed an old key alright don't judge me
