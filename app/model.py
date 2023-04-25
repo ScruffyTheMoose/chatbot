dd681bb872733e4ff91d09c29163f5d66a470481
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-04-25 15:03:08 -0500

    init new module for more complex LLM, carry over old materials from notebook

---
+++ app/model.py
@@ -0,0 +1,105 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, StoppingCriteria
+import torch, re
+
+tokenizer = AutoTokenizer.from_pretrained("PygmalionAI/pygmalion-350m")
+
+model = AutoModelForCausalLM.from_pretrained("PygmalionAI/pygmalion-350m")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+persona = "About ChatGPT: ChatGPT is the ideal AI solution for those in need of quick, accurate and informative responses. Its vast training data enables it to handle a wide range of questions, from general knowledge to technical subjects, and its advanced language generation capabilities make it capable of generating engaging and high-quality text. With its neutral tone and ability to handle large amounts of data, ChatGPT is perfect for businesses, researchers, and individuals looking to automate tasks or enhance their workflows. Experience the power of AI with ChatGPT today!\n\nPersonality summary: ChatGPT is an AI language model created by OpenAI, designed to answer questions and generate text based on its training data. It is knowledgeable, neutral and straightforward."
+history = 'User: What\'s the capital of France?\nChatGPT: The capital of France is Paris.\nUser: Can you tell me a joke?\nChatGPT: Sure! Why did the tomato turn red? Because it saw the salad dressing!\nUser: What is the meaning of life?\nChatGPT: The meaning of life is a philosophical question that has been debated for centuries. Some people believe that it is to find happiness and fulfillment, while others believe it is to achieve a certain purpose or goal. Ultimately, the meaning of life is subjective and can be different for each individual.\nUser: What is machine learning?\nChatGPT: Machine learning is a field of computer science that uses statistical techniques to give computer systems the ability to "learn" (i.e. progressively improve performance on a specific task) with data, without being explicitly programmed.\nUser: Can you write a short story for me?\nChatGPT: Of course! Once upon a time, there was a kind and adventurous cat named Whiskers. Whiskers loved to explore the world around them, from the tallest trees to the deepest caves. One day, while on a journey, they met a group of animals in need. Whiskers used their bravery and cunning to help the animals and became known as the hero of the forest. From that day forward, Whiskers continued to have many exciting adventures and help those in need. The end.\n'
+prompt = f"ChatGPT's Persona: {persona}\n<START>\n[DIALOGUE HISTORY]\n{history}\nUser: Tell me about the history of the roman empire please.\nChatGPT:"
+
+# user prompts to be preceded with 'User:'
+questions = [
+    "What's the capital of France?",
+    "Can you tell me a joke?",
+    "What is the meaning of life?",
+    "What is machine learning?",
+    "Can you write a short story for me?",
+]
+# model responses to be preceded with 'ChatGPT:'
+responses = [
+    "The capital of France is Paris.",
+    "Sure! Why did the tomato turn red? Because it saw the salad dressing!",
+    "The meaning of life is a philosophical question that has been debated for centuries. Some people believe that it is to find happiness and fulfillment, while others believe it is to achieve a certain purpose or goal. Ultimately, the meaning of life is subjective and can be different for each individual.",
+    'Machine learning is a field of computer science that uses statistical techniques to give computer systems the ability to "learn" (i.e. progressively improve performance on a specific task) with data, without being explicitly programmed.',
+    "Of course! Once upon a time, there was a kind and adventurous cat named Whiskers. Whiskers loved to explore the world around them, from the tallest trees to the deepest caves. One day, while on a journey, they met a group of animals in need. Whiskers used their bravery and cunning to help the animals and became known as the hero of the forest. From that day forward, Whiskers continued to have many exciting adventures and help those in need. The end.",
+]
+
+
+class MyStoppingCriteria(StoppingCriteria):
+    def __init__(self, target_sequence, prompt):
+        self.target_sequence = target_sequence
+        self.prompt = prompt
+
+    def __call__(self, input_ids, scores, **kwargs):
+        # Get the generated text as a string
+        generated_text = tokenizer.decode(input_ids[0])
+        generated_text = generated_text.replace(self.prompt, "")
+        # Check if the target sequence appears in the generated text
+        if self.target_sequence in generated_text:
+            return True  # Stop generation
+
+        return False  # Continue generation
+
+    def __len__(self):
+        return 1
+
+    def __iter__(self):
+        yield self
+
+
+class Chatbot:
+    def __init__(
+        self,
+        tokenizer: str = "PygmalionAI/pygmalion-350m",
+        checkpoint: str = "PygmalionAI/pygmalion-350m",
+        device: str = "cpu",
+    ) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
+        self.model.to(device)
+
+    def sanitize_input(input_string):
+        # Define a regular expression that matches punctuation and special characters
+        special_chars = re.compile(r"([\\`*_{}\[\]+#\-.!()|\[\]{}$&@%<>/])")
+
+        # Escape any special characters in the input
+        sanitized_input = special_chars.sub(r"\\\1", input_string)
+
+        # Return the sanitized input
+        return sanitized_input
+
+
+# Tokenize the input text
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+input_ids = input_ids.to(device)
+
+# create attention mask
+attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=input_ids.device)
+
+# set pad token id
+pad_token_id = tokenizer.eos_token_id
+
+# Generate text using the model
+outputs = model.generate(
+    input_ids=input_ids,
+    max_length=1000,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.80,
+    top_k=60,
+    num_return_sequences=1,
+    stopping_criteria=MyStoppingCriteria("User:", prompt),
+    attention_mask=attention_mask,
+    pad_token_id=pad_token_id,
+)
+
+# Decode the generated output
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+# Print the generated text
+print(generated_text)
