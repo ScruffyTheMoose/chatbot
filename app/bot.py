61aaa493f745486cfc1d063e25b90416a814eeee
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-04-25 19:37:54 -0500

    add detailed comments for clarity

--- app/bot.py
+++ app/bot.py
@@ -5,33 +5,40 @@ from characters import ChatGPT
 # loading in ChatGPT details from characters module
 character = ChatGPT()
 
+# opening all intents except for privilaged
 intents = discord.Intents.all()
 intents.members = False
 intents.presences = False
 
+# instantiating discord client
 client = discord.Client(intents=intents)
+
+# instantiating chatbot
 chatbot = Chatbot(
-    tokenizer="PygmalionAI/pygmalion-350m",
+    tokenizer="PygmalionAI/pygmalion-350m",  # using the smallest PygmalionAI model for best CPU performance
     checkpoint="PygmalionAI/pygmalion-350m",
-    device="cpu",
-    persona=character["persona"],
-    questions=character["questions"],
-    responses=character["responses"],
-    max_sequences=20,
+    device="cpu",  # selecting which device the model is computed on - can use code <"cuda" if torch.cuda.is_available() else "cpu"> or can explicitly select a device
+    persona=character["persona"],  # persona from our selected character
+    questions=character["questions"],  # questions list from our selected character
+    responses=character["responses"],  # responses list from our selected character
+    max_sequences=30,  # number of sequence pairs (question : response) that it will keep track of. The more memory available (RAM or VRAM), the more you can track.
 )
 
 
 @client.event
 async def on_ready():
+    # chatbot is now ready for inputs
     print("Logged in as {0.user}".format(client))
 
 
 @client.event
 async def on_message(message):
 
+    # chatbot will not respond to itself
     if message.author == client.user:
         return
 
+    # chatbot responds only to messages beginning with key '~!'
     if message.content.startswith("~!"):
 
         # remove '~!' from start of message and sanitizes contents
